94b2d60b759527b188cd4427969c92469deb602c
LucasSchurer <lschurerm@gmail.com>
2024-12-06 00:57:32 -0300

    Create scheduler

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ from cbr import CBR
 from dataset import heart_disease
 import json
 import numpy as np
+from datetime import datetime
 
 
-def evaluate_performance(cbr, test_set, runs_json=None, run_name=None):
+def evaluate_performance(
+    cbr,
+    test_set,
+    runs_json=None,
+    run_name=None,
+    retrieved_cases=5,
+    runs_filename="runs.json",
+):
     y_true = []
     y_pred = []
 
@@ -15,7 +23,7 @@ def evaluate_performance(cbr, test_set, runs_json=None, run_name=None):
         query = row.to_dict()
         true_label = query.pop("target")
         result = cbr.retrieve(query)
-        similar_cases = result.ranking[:5]
+        similar_cases = result.ranking[:retrieved_cases]
         labels = [result.casebase[case]["target"] for case in similar_cases]
 
         values, counts = np.unique(labels, return_counts=True)
@@ -47,63 +55,76 @@ def evaluate_performance(cbr, test_set, runs_json=None, run_name=None):
 
         runs_json[run_name]["pooling_weights"] = cbr.pooling_weights
 
-        with open("runs.json", "w") as f:
+        with open(runs_filename, "w") as f:
             json.dump(runs_json, f)
 
 
 def main():
-    runs_json = json.load(open("runs.json", "r"))
-    run_name = ""
-    description = ""
-    stratified = True
-    use_ga_optimizer = True
+    scheduler = json.load(open("scheduler.json", "r"))
 
-    runs_json[run_name] = {}
+    timestamp = datetime.now().strftime("%Y-%m-%d-%H_%M_%S")
 
-    train_set, validation_set, test_set, config, pooling_weights = heart_disease(
-        validation=True, stratify=stratified
-    )
+    runs_json = {}
+    runs_filename = f"runs_{timestamp}.json"
 
-    case_base = {idx: row.to_dict() for idx, row in train_set.iterrows()}
+    for run in scheduler["runs"]:
+        run_name = run["name"]
+        description = run["description"]
+        stratified = run["stratified"]
+        use_ga_optimizer = run["use_ga_optimizer"]
+        retrieved_cases = run["retrieved_cases"]
+        ga_config = run.get("ga_config", None)
+        initial_weights = run.get("initial_weights", None)
 
-    validation_case_base = {
-        idx: row.to_dict() for idx, row in validation_set.iterrows()
-    }
+        runs_json[run_name] = {}
 
-    ga_config = {
-        "generations": 10,
-        "metric": "accuracy",
-        "population_config": {
-            "population_size": 70,
-            "mutation_rate": 0.3,
-            "mutate_individually": True,
-            "k_elitism": 15,
-            "tournament_size": 20,
-        },
-    }
+        train_set, validation_set, test_set, config, pooling_weights = heart_disease(
+            validation=True, stratify=stratified
+        )
 
-    cbr = CBR(
-        case_base=case_base,
-        validation_case_base=validation_case_base,
-        k_cases=5,
-        config=config,
-        use_ga_optimizer=use_ga_optimizer,
-        ga_config=ga_config,
-        pooling_weights=pooling_weights,
-        run_dict=runs_json[run_name],
-    )
+        case_base = {idx: row.to_dict() for idx, row in train_set.iterrows()}
 
-    runs_json[run_name]["description"] = description
-    runs_json[run_name]["stratified"] = stratified
-    runs_json[run_name]["use_ga_optimizer"] = use_ga_optimizer
+        validation_case_base = {
+            idx: row.to_dict() for idx, row in validation_set.iterrows()
+        }
 
-    if use_ga_optimizer:
-        runs_json[run_name]["ga_config"] = ga_config
+        if initial_weights is not None:
+            if isinstance(initial_weights, float) or isinstance(initial_weights, int):
+                pooling_weights = {
+                    feature: initial_weights for feature in pooling_weights
+                }
+        else:
+            pooling_weights = None
 
-    evaluate_performance(cbr, test_set, runs_json, run_name)
+        cbr = CBR(
+            case_base=case_base,
+            validation_case_base=validation_case_base,
+            k_cases=retrieved_cases,
+            config=config,
+            use_ga_optimizer=use_ga_optimizer,
+            ga_config=ga_config,
+            pooling_weights=pooling_weights,
+            run_dict=runs_json[run_name],
+        )
 
-    print("\n")
-    print(cbr.pooling_weights)
+        runs_json[run_name]["description"] = description
+        runs_json[run_name]["stratified"] = stratified
+        runs_json[run_name]["use_ga_optimizer"] = use_ga_optimizer
+
+        if use_ga_optimizer:
+            runs_json[run_name]["ga_config"] = ga_config
+
+        evaluate_performance(
+            cbr,
+            test_set,
+            runs_json,
+            run_name,
+            retrieved_cases=retrieved_cases,
+            runs_filename=runs_filename,
+        )
+
+        print("\n")
+        print(cbr.pooling_weights)
 
 
 if __name__ == "__main__":
